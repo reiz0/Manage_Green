a7bcb49c01f6ec02a9ff82e19b0648b9197c6cfa
yuki <yukihiguchi0826@gmail.com>
2024-10-03 20:14:42 -0700

    task group layout done

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
 
+import { baseText } from "@/app/style/baseClass";
 import { FaPlus } from "react-icons/fa6";
 import { Group } from "./_group.component";
-import { baseText } from "@/style/baseClass";
 
 export type groupInfoType = {
   title: string;
@@ -23,25 +23,29 @@ export default function Page() {
       title: "Shopping List",
       members: ["dog", "cat"],
     },
+    {
+      title: "gaming",
+      members: ["dog", "lion"],
+    },
   ];
   return (
-    <div>
-      <h1 className={`text-4xl font-bold ${baseText}`}>My Task Groups</h1>
-      <div>
-        <Button
-          className=" bg-slate-50 hover:bg-green-100 w-96 h-36 flex flex-col" asChild
-        >
-          <div className={`text-2xl ${baseText}`}>
-            <FaPlus />
-            <p>Create New Group</p>
-          </div>
-        </Button>
-        {groups.map((group, index) => {
-          console.log(index);
-          return(
+    <div className="text-center">
+      <h1 className={`text-4xl font-bold ${baseText} mb-10 mt-5`}>My Task Groups</h1>
+      <div className="grid place-items-center">
+        <div className="grid gap-3 md:grid-cols-2 xl:grid-cols-3">
+          <Button
+            className="bg-slate-50 hover:bg-green-100 w-96 h-36 flex flex-col text-2xl"
+            asChild
+          >
+            <div className={`${baseText}`}>
+              <FaPlus />
+              <p>Create New Group</p>
+            </div>
+          </Button>
+          {groups.map((group, index) => (
             <Group key={index} index={index} groupInfo={group} />
-          )
-        })}
+          ))}
+        </div>
       </div>
     </div>
   );
