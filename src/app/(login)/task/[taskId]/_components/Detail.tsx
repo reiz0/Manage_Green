2be91a5c6696542409d2d539b360b05a7b928be8
yuki <yukihiguchi0826@gmail.com>
2024-10-16 23:23:41 -0700

    implement Details component

---
+++ src/app/(login)/task/[taskId]/_components/Detail.tsx
@@ -0,0 +1,12 @@
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export const Detail = () => {
+  return (
+    <>
+      <CardHeader>
+        <CardTitle>Details</CardTitle>
+      </CardHeader>
+      <CardContent></CardContent>
+    </>
+  );
+};
