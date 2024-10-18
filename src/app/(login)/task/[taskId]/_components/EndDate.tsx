3880e7451ee773399a22175ff769160c60f17daf
yuki <yukihiguchi0826@gmail.com>
2024-10-17 17:50:03 -0700

    implement StartDate and EndDate

---
+++ src/app/(login)/task/[taskId]/_components/EndDate.tsx
@@ -0,0 +1,5 @@
+import { DatePicker } from "./DatePicker";
+
+export const EndDate = () => {
+  return <DatePicker title="End Date" />;
+};
--- src/app/(login)/task/[taskId]/_components/StartDate.tsx
+++ src/app/(login)/task/[taskId]/_components/StartDate.tsx
@@ -1,50 +1,10 @@
-"use client"
-import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
+import { DatePicker } from "./DatePicker";
 
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 
-import { cn } from "@/lib/utils";
-import { useState } from "react";
+
 
 export const StartDate = () => {
-  const [date, setDate] = useState<Date>();
-  return (
-    <>
-      <CardHeader>
-        <CardTitle>Start Date</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              variant={"outline"}
-              className={cn(
-                "w-[280px] justify-start text-left font-normal",
-                !date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              {date ? format(date, "PPP") : <span>Pick a date</span>}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0">
-            <Calendar
-              mode="single"
-              selected={date}
-              onSelect={setDate}
-              initialFocus
-            />
-          </PopoverContent>
-        </Popover>
-      </CardContent>
-    </>
-  );
+  return(
+    <DatePicker title="Start Date" />
+  )
 };
