470c11d65d4c78c352afe5a0916f1cfd7ca44538
yuki <yukihiguchi0826@gmail.com>
2024-10-25 19:00:11 -0700

    implement bold, italic, strile button for edit

--- src/app/(login)/task/[taskId]/_components/Editor.tsx
+++ src/app/(login)/task/[taskId]/_components/Editor.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import Underline from "@tiptap/extension-underline";
+// import BulletList from '@tiptap/extension-bullet-list'
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+
+import { FaBold, FaItalic, FaStrikethrough, FaUnderline } from "react-icons/fa";
+import { MdFormatListBulleted } from "react-icons/md";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -15,11 +19,51 @@ export const Editor = () => {
     content: "<p>Hello World! 🌎️</p>",
   });
 
+  // StarterKit.configure({
+  //   bulletList:{
+  //     keepMarks: true,
+  //     HTMLAttributes:{
+  //       class: "list-disc"
+  //     }
+  //   }
+  // })
+  
+  if (!editor) {
+    return (
+      <div className="border-2 rounded-md w-full h-[600px] overflow-y-scroll overflow-hidden " />
+    );
+  }
+
+  const edditorButtonClass = "p-1 text-3xl rounded-md"
+  const activeClass = `bg-slate-200 ${edditorButtonClass}`;
+
   return (
     <div className="">
+      <div className="flex gap-1 items-center py-1">
+        <FaBold
+          className={editor.isActive("bold") ? activeClass : edditorButtonClass }
+          onClick={() => editor.chain().focus().toggleBold().run()}
+        />
+        <FaItalic
+          className={editor.isActive("italic") ? activeClass : edditorButtonClass}
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+        />
+        <FaStrikethrough
+          className={editor.isActive("strike") ? activeClass : edditorButtonClass}
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+        />
+        <FaUnderline
+          className={editor.isActive("underline") ? activeClass : edditorButtonClass}
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+        />
+        <MdFormatListBulleted
+          className={editor.isActive("bulletList") ? activeClass : edditorButtonClass}
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+        />
+      </div>
       <div
         className="border-2 rounded-md w-full h-[600px] overflow-y-scroll overflow-hidden"
-        onClick={() => editor?.chain().focus()}
+        onClick={() => editor.chain().focus()}
       >
         <EditorContent editor={editor} />
       </div>
